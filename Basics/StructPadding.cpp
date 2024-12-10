1348ad2012b6b8782f0ba37247ee6bee3a9841da
UjjawalkTjNw <ujjawal.singh467@gmail.com>
2024-12-10 22:48:39 +0530

    struct padding

--- Basics/StructPadding.cpp
+++ Basics/StructPadding.cpp
@@ -1,3 +1,9 @@
+
+//Example 1: Normal data type
+/*
+|1 byte a | 3 byte padding | 4 byte b | 1 byte c
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -14,3 +20,27 @@ int main() {
     cout << "Size of struct Example: " << sizeof(ex) << " bytes" << endl;
     return 0;
 }
+
+
+
+
+
+//Example2: with arrays
+/*
+| 10byte arr1 | 2 byte padding | 40 byte arr2
+
+*/
+#include <iostream>
+using namespace std;
+
+struct Example {
+    char arr1[10]; // 10 bytes
+    int arr2[10];  // 40 bytes
+};
+
+int main() {
+    Example ex;
+    cout << "Size of struct Example: " << sizeof(ex) << " bytes" << endl;
+    return 0;
+}
+
