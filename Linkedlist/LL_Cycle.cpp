d90695a668bf26f49985f2bd904905e8c3b4d2d6
UjjawalkTjNw <ujjawal.singh467@gmail.com>
2024-12-29 00:13:00 +0530

    loop in ll

--- Linkedlist/LL_Cycle.cpp
+++ Linkedlist/LL_Cycle.cpp
@@ -1,21 +1,48 @@
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        if(head==NULL)
-            return false;
-        //make two pointers and put them at head of LL
-        ListNode* fast = head;
-        ListNode* slow = head;
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
-            slow=slow->next;
-            
-            
-            if(fast==slow)
-                return true;
-        }
-        return false ;
-        
-        
-    }
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
+
+bool isCircular(ListNode* head) {
+    if (head == NULL) return false;
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    // Creating a circular linked list
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = head; // Creating a circular link
+
+    if (isCircular(head)) {
+        cout << "The linked list is circular." << endl;
+    } else {
+        cout << "The linked list is not circular." << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+//This approach efficiently detects if a linked list is circular with a time complexity of \(O(n)\) and a space complexity of \(O(1)\).
