e9be8c1733bfadd3a90f02f6f652d9a6f58ad62f
UjjawalkTjNw <ujjawal.singh467@gmail.com>
2024-12-29 00:06:36 +0530

    sizeof implementation

--- Basics/SizeOfImplementation.cpp
+++ Basics/SizeOfImplementation.cpp
@@ -14,3 +14,27 @@ int main() {
     return 0;
 }
 
+
+
+
+
+//code 1: in c language
+#include <stdio.h>
+#define my_sizeof(type) (char *)(&type+1)-(char*)(&type)
+
+int main() {
+    double x;
+    printf("%ld\n", my_sizeof(x));
+    return 0;
+}
+
+
+//code 2: in cpp
+#include <iostream>
+#define my_sizeof(type) (char *)(&type+1)-(char*)(&type)
+
+int main() {
+    double x;
+    std::cout << my_sizeof(x) << std::endl;
+    return 0;
+}
